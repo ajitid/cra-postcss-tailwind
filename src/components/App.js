e33a4cc21b77f95dcefdbb437165c499ede04348
Ajit Singh <ajit.singh2905@gmail.com>
2018-08-15 01:03:21 +0530

    init

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="font-sans">
+        <div className="flex justify-center">
+          <h1 className="text-indigo font-light mt-4">All set! <span role="img" aria-label="thumbs up">👍</span></h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
